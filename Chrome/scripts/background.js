1c75999393dc921c82181c86e2f87138f1778e91
Devin Rousso <dcrousso@gmail.com>
2015-02-09 21:08:56 -0800

    Added check to see if message passed to extension is from BackChain

--- Chrome/scripts/background.js
+++ Chrome/scripts/background.js
@@ -1,9 +1,10 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-		if(changeInfo.status == 'complete') {
+		if(changeInfo.status == 'complete' && request.backChainURL.length > 0) {
 			chrome.tabs.query({url: "*:" + request.backChainURL}, function(tabs) {
 				if(tabs.length == 1) {
 					chrome.tabs.sendMessage(tabs[0].id, {backChainOriginURL: sender.url});
+					request.backChainURL = "";
 				}
 			});
 		}
